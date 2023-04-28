bd0c9c223c3c8fd324ad26b3f7965b57cb4ac858
akh-ild <ildar.akhmadeyev@gmail.com>
2023-04-28 12:44:30 +0600

    sidebar style, transition

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,88 @@
+<template>
+  <aside :class="`${ isExpanded && 'is_expanded' }`">
+    <div class="logo">
+      <img src="../assets/logo.jpg" alt="Vue">
+    </div>
+    <div class="menu-toggle-wrap">
+      <button class="menu-toggle" @click="toggleMenu">
+        <span class="material-icons">keyboard_double_arrow_right</span>
+      </button>
+    </div>
+  </aside>
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+const isExpanded = ref(false)
+
+const toggleMenu = () => {
+  isExpanded.value = !isExpanded.value
+}
+</script>
+
+<style lang="scss" scoped>
+aside {
+  display: flex;
+  flex-direction: column;
+  width: calc(2rem + 32px);
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 1rem;
+
+  background-color: var(--dark);
+  color: var(--light);
+
+  transition: 0.2s ease-out;
+
+  &.is_expanded {
+    width: var(--sidebar-width);
+
+    .menu-toggle-wrap {
+      top: -3rem;
+
+      .menu-toggle {
+        transform: scaleX(-1);
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    position: fixed;
+    z-index: 99;
+  }
+
+  .logo {
+    margin-bottom: 1rem;
+
+    img {
+      width: 2rem;
+    }
+  }
+
+  .menu-toggle-wrap {
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 1rem;
+    position: relative;
+    top: 0;
+    transition: 0.2s ease-out;
+
+    .menu-toggle {
+      transition: 0.2s ease-out;
+      &:hover {
+        .material-icons {
+          color: var(--primary);
+          transform: translateX(0.5rem);
+        }
+      }
+
+      .material-icons {
+        font-size: 2rem;
+        color: var(--light);
+        transition: 0.2s ease-out;
+      }
+    }
+  }
+}
+</style>
