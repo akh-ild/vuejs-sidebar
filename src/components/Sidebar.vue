969199c889e05e269338001ad1fda87cf8ba8c1b
akh-ild <ildar.akhmadeyev@gmail.com>
2023-04-30 21:51:19 +0600

    menu items

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -8,6 +8,32 @@
         <span class="material-icons">keyboard_double_arrow_right</span>
       </button>
     </div>
+    <h3>Menu</h3>
+    <div class="menu">
+      <router-link class="button" to="/">
+        <span class="material-icons">home</span>
+        <span class="text">Home</span>
+      </router-link>
+      <router-link class="button" to="/about">
+        <span class="material-icons">visibility</span>
+        <span class="text">About</span>
+      </router-link>
+      <router-link class="button" to="/team">
+        <span class="material-icons">group</span>
+        <span class="text">Team</span>
+      </router-link>
+      <router-link class="button" to="/contact">
+        <span class="material-icons">email</span>
+        <span class="text">Contact us</span>
+      </router-link>
+    </div>
+    <div class="flex"></div>
+    <div class="menu">
+      <router-link class="button" to="/settings">
+        <span class="material-icons">settings</span>
+        <span class="text">Settings</span>
+      </router-link>
+    </div>
   </aside>
 </template>
 
@@ -43,6 +69,16 @@ aside {
 
       .menu-toggle {
         transform: scaleX(-1);
+      }
+    }
+
+    h3, .button .text {
+      opacity: 1;
+    }
+
+    .button {
+      .material-icons {
+        margin-right: 1rem;
       }
     }
   }
@@ -83,6 +119,59 @@ aside {
         transition: 0.2s ease-out;
       }
     }
+  }
+
+  h3, .button .text {
+    opacity: 0;
+    transition: 0.3s ease-out;
+  }
+
+  h3 {
+    color: var(--grey);
+    font-size: 0.875rem;
+    margin-bottom: 0.5rem;
+    text-transform: uppercase;
+  }
+
+  .menu {
+    margin: 0 -1rem;
+
+    .button {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+      padding: 0.5rem 1rem;
+      transition: 0.2s ease-out;
+
+      .material-icons {
+        font-size: 2rem;
+        color: var(--light);
+        transition: 0.2s ease-out;
+      }
+
+      .text {
+        color: var(--light);
+        transition: 0.2s ease-out;
+      }
+
+      &:hover, &.router-link-exact-active {
+        background-color:var(--dark-alt);
+
+        .material-icons, .text {
+          color: var(--primary);
+        }
+
+      }
+
+      &.router-link-exact-active {
+        border-right: 5px solid var(--primary);
+      }
+      
+    }
+  }
+
+  .flex {
+    flex: 1 1 0;
   }
 }
 </style>
